1a281b65b5b55b83f8d2680d5fcb2ba0224c5a03
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-02-25 16:48:47 +0100

    A propos : add the techs icons into the page

---
+++ src/data/a-propos.ts
@@ -0,0 +1,83 @@
+import flexibleIcon from '@/assets/flexible.svg';
+import securityIcon from '@/assets/security.svg';
+import durabilityIcon from '@/assets/durability.svg';
+
+import wordpressIcon from '@/assets/techs/wordpress.svg';
+import nextjsIcon from '@/assets/techs/nextjs.svg';
+import astroIcon from '@/assets/techs/astro.svg';
+import reactIcon from '@/assets/techs/react.svg';
+import shopifyIcon from '@/assets/techs/shopify.svg';
+import woocommerceIcon from '@/assets/techs/woocommerce.svg';
+import strapiIcon from '@/assets/techs/strapi.svg';
+import threejsIcon from '@/assets/techs/threejs.svg';
+
+export const methodology = [
+  {
+    title: 'Conception flexible',
+    text: 'Anim eu eiusmod incididunt proident aliqua sit sit laborum. Adipisicing ullamco do fugiat duis reprehenderit deserunt eiusmod quis aliquip elit pariatur.',
+    icon: flexibleIcon,
+  },
+  {
+    title: 'Des sites web sécurisés',
+    text: 'Anim eu eiusmod incididunt proident aliqua sit sit laborum. Adipisicing ullamco do fugiat duis reprehenderit deserunt eiusmod quis aliquip elit pariatur.',
+    icon: securityIcon,
+  },
+  {
+    title: 'Construction durable',
+    text: 'Anim eu eiusmod incididunt proident aliqua sit sit laborum. Adipisicing ullamco do fugiat duis reprehenderit deserunt eiusmod quis aliquip elit pariatur.',
+    icon: durabilityIcon,
+  },
+];
+
+export const techs = [
+  {
+    label: 'Wordpress',
+    icon: wordpressIcon,
+  },
+  {
+    label: 'Next JS',
+    icon: nextjsIcon,
+  },
+  {
+    label: 'Astro JS',
+    icon: astroIcon,
+  },
+  {
+    label: 'React',
+    icon: reactIcon,
+  },
+  {
+    label: 'Shopify',
+    icon: shopifyIcon,
+  },
+  {
+    label: 'Woocommerce',
+    icon: woocommerceIcon,
+  },
+  {
+    label: 'Strapi',
+    icon: strapiIcon,
+  },
+  {
+    label: 'Three.js',
+    icon: threejsIcon,
+  },
+];
+
+export const questions = [
+  {
+    label: 'Quelle est la taille de votre équipe ?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    label: 'Quelle est la taille de votre équipe ?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    label: 'Comment fonctionne votre équipe ?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
